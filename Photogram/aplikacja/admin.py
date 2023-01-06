654bfffe149884044d97645ad1efe86fe0d038d1
Dejwer <dawid1476321@gmail.com>
2023-01-06 20:08:37 +0100

    add decorators in admin.py

--- Photogram/aplikacja/admin.py
+++ Photogram/aplikacja/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Profile, Image, Like, Comment
 
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Image)
-admin.site.register(Like)
-admin.site.register(Comment)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'age', 'bio', 'status']
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['description']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text']
--- Photogram/aplikacja/models.py
+++ Photogram/aplikacja/models.py
@@ -8,16 +8,22 @@ PROFILE_STATUS=(
 
 class Profile(models.Model):
     user_name = models.TextField(max_length = 50)
-    age = models.IntegerField(max_length=3)
+    age = models.IntegerField()
     bio = models.TextField(max_length= 200)
     #profile_img = models.ImageField()
     status = models.CharField(choices=PROFILE_STATUS, max_length=30, default='unfollowed')
 
+
 class Image(models.Model):
     description = models.TextField(max_length= 400)
+
+    def __str__(self):
+        return self.description
+
 
 class Like(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
 
+
 class Comment(models.Model):
-    text = models.TextField(max_length= 2137)
+    text = models.TextField(max_length=500)
