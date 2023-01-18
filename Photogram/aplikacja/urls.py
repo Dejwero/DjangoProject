9241a0562b664c76f3c80d3998903821a95bc34e
user <user@DESKTOP-RVKN333>
2023-01-19 00:17:25 +0100

    I created url routes like - mainpage, login, signup, profile - and basic templates for them. I created route which I called photogram - this is page which will be shown before logging in

--- Photogram/aplikacja/urls.py
+++ Photogram/aplikacja/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.photogram, name='photogram'),
+    path('mainpage/', views.mainpage, name='mainpage-photogram'),
+    path('login/', views.login, name='log-in'),
+    path('signup/', views.signup, name='sign-up'),
+    path('profile/', views.profile, name='profile')
 ]
--- Photogram/aplikacja/views.py
+++ Photogram/aplikacja/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse('<h1>PhotoGram</h1>')
+def photogram(request):
+    return render(request, 'photogram.html')
+def mainpage(request):
+    return render(request, 'mainpage.html')
+def login(request):
+    return render(request, 'login.html')
+def signup(request):
+    return render(request, 'signup.html')
+def profile(request):
+    return render(request, 'profile.html')
