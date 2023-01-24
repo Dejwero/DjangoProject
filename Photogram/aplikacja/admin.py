151d1570ebceeeb8e65c595b6b228ed190d399a5
Dejwer <dawid1476321@gmail.com>
2023-01-25 00:47:25 +0100

    change in Like, Profile and Image

--- Photogram/aplikacja/admin.py
+++ Photogram/aplikacja/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from .models import Profile, Image, Like, Comment
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user_name', 'age', 'bio', 'status']
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['description']
+    list_display = ['description', 'profile']
+    inlines = [CommentInline]
 
 
 @admin.register(Like)
@@ -17,4 +21,4 @@ class LikeAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['text']
+    list_display = ['text', 'image']
