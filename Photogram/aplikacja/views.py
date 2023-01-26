2ae5ee62436cbbf31103fc50ef6da9679b38c47a
user <user@DESKTOP-RVKN333>
2023-01-26 20:07:31 +0100

    bad

--- Photogram/aplikacja/views.py
+++ Photogram/aplikacja/views.py
@@ -3,28 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.shortcuts import render, redirect
 
-posts = [
-    {
-        'nickname': 'martunia880',
-        'photo': 'example photo',
-        'content': 'My irst post',
-        'date': '23/02/2022'
-    },
-    {
-        'nickname': 'dejwero',
-        'photo': 'examle photo',
-        'content': 'First post',
-        'date': '21/02/2022'
-    }
-]
 
 def photogram(request):
     return render(request, 'photogram.html', {'title': 'Witamy!'})
 def mainpage(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'mainpage.html', context, {'title': 'Strona główna'})
+    return render(request, 'mainpage.html', {'title': 'Strona główna'})
 def login(request):
     return render(request, 'login.html', {'title': 'Zaloguj się'})
 def signup(request):
